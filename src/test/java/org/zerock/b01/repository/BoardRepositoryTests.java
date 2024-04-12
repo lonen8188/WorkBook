2f767c1b797ffd0d8c95b6ec1d0e9cc6004d2486
10-5\kkw <lonen@nate.com>
2024-04-12 11:16:43 +0900

    BoardController

---
+++ src/test/java/org/zerock/b01/repository/BoardRepositoryTests.java
@@ -0,0 +1,151 @@
+package org.zerock.b01.repository;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.zerock.b01.domain.Board;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+@SpringBootTest
+@Log4j2
+public class BoardRepositoryTests {
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+    @Test
+    public void testInsert() {
+        IntStream.rangeClosed(1,100).forEach(i -> {
+            Board board = Board.builder()
+                    .title("title..." +i)
+                    .content("content..." + i)
+                    .writer("user"+ (i % 10))
+                    .build();
+
+            Board result = boardRepository.save(board);
+            log.info("BNO: " + result.getBno());
+        });
+    }
+
+    @Test
+    public void testSelect() {
+        Long bno = 100L;
+
+        Optional<Board> result = boardRepository.findById(bno);
+        // findById()을 리턴 타입은 Optional 참조하더라도 NPE가 발생하지 않도록 도와준다.
+        // Optional 클래스는 아래와 같은 value에 값을 저장하기 때문에 값이 null이더라도 바로 NPE가 발생하지 않으며, 클래스이기 때문에 각종 메소드를 제공해준다.
+        // NPE : 널포인트익셉션
+        // Optional.empty() -> 널값인 경우,  Optional.of() -> 널값이 아닌 경우, Optional.ofNullbale() - 값이 Null일수도, 아닐수도 있는 경우
+
+        Board board = result.orElseThrow();
+        // Optional 객체의 유무를 판단하고 예외를 처리하기 위해 if문을 사용해왔습니다.
+        // if문을 사용하면서 예외 처리 또는 값을 반환하다보니 코드의 가독성이 떨어졌습니다.
+        // orElseThrow를 통해 Optional에서 원하는 객체를 바로 얻거나 예외를 던질 수 있습니다.
+
+        log.info(board);
+
+    }
+
+    @Test
+    public void testUpdate() {
+
+        Long bno = 100L;
+
+        Optional<Board> result = boardRepository.findById(bno);
+
+        Board board = result.orElseThrow();
+
+        board.change("update..title 100", "update content 100");
+
+        boardRepository.save(board);
+
+    }
+
+    @Test
+    public void testDelete() {
+        Long bno = 1L;
+
+        boardRepository.deleteById(bno);
+    }
+
+    @Test
+    public void testPaging() {
+
+        //1 page order by bno desc
+        Pageable pageable = PageRequest.of(0,10, Sort.by("bno").descending());
+
+        Page<Board> result = boardRepository.findAll(pageable);
+
+
+        log.info("total count: "+result.getTotalElements());
+        log.info( "total pages:" +result.getTotalPages());
+        log.info("page number: "+result.getNumber());
+        log.info("page size: "+result.getSize());
+
+        List<Board> todoList = result.getContent();
+
+        todoList.forEach(board -> log.info(board));
+
+
+    }
+
+    @Test
+    public void testSearch1() {
+
+        //2 page order by bno desc
+        Pageable pageable = PageRequest.of(1,10, Sort.by("bno").descending());
+
+        boardRepository.search1(pageable);
+
+    }
+
+    @Test
+    public void testSearchAll() {
+
+        String[] types = {"t","c","w"};
+
+        String keyword = "1";
+
+        Pageable pageable = PageRequest.of(0,10, Sort.by("bno").descending());
+
+        Page<Board> result = boardRepository.searchAll(types, keyword, pageable );
+
+    }
+
+    @Test
+    public void testSearchAll2() {
+
+        String[] types = {"t","c","w"};
+
+        String keyword = "1";
+
+        Pageable pageable = PageRequest.of(0,10, Sort.by("bno").descending());
+
+        Page<Board> result = boardRepository.searchAll(types, keyword, pageable );
+
+        //total pages
+        log.info(result.getTotalPages());
+
+        //pag size
+        log.info(result.getSize());
+
+        //pageNumber
+        log.info(result.getNumber());
+
+        //prev next
+        log.info(result.hasPrevious() +": " + result.hasNext());
+
+        result.getContent().forEach(board -> log.info(board));
+    }
+
+
+
+}
